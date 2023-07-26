1bc9ff6296992c5bfab66853ef2d2284387c7d2e
MD Rakib Khan <contact.antor.bd@gmail.com>
2023-07-27 00:02:02 +0600

    card creat

--- src/components/Achieve/Achieve.jsx
+++ src/components/Achieve/Achieve.jsx
@@ -1,26 +1,28 @@
-import me from "../../assets/Group1.png";
+// import me from "../../assets/Group1.png";
+import '../../App.css'
+import group1 from '../../assets/Group1.png'
 const Achieve = () => {
   const data = [
     {
-      img: me,
+      img: group1,
       title: "Main Examination",
       details:
         "Geography is one of the most popular options. It owes this to its multidisciplinary characteristics.",
     },
     {
-      img: me,
+      img: group1,
       title: "Test Series",
       details:
         "Geography Test series to provide answer framing and time management skills.",
     },
     {
-      img: me,
+      img: group1,
       title: "Postal Programme",
       details:
         "The institute in its commitment of extending support to the aspirants has initiated correspondence guidelines.",
     },
     {
-      img: me,
+      img: group1,
       title: "GS (Modules)",
       details:
         "General Studies Geography is divided into 3 parts including pre-cum-main",
@@ -29,29 +31,127 @@ const Achieve = () => {
 
   return (
     <div>
-      <h1 className="text-center fs-1 my-5">
-        Achieve Excellence with <br /> Our Programmes
-      </h1>
-      <div className="">
-        {data.map((data, index) => {
-          return (
-            <div key={index} className="col-lg-6 col-md-6">
-              <div className="row">
-                <div className="left card-bg"></div>
-                <div className="right"></div>
-                {/* <div className="col card-img">
-                  <img src={data.img} alt="img" />
+      <div>
+        <h1 className="text-center fs-1 my-5">
+          Achieve Excellence with <br /> Our Programmes
+        </h1>
+
+        <div className="parent-container">
+          {
+            data.map(item => (<>
+              <div className="card-wrapper">
+                <div className='card-container-box'>
+                  <img className='card-icon-img' src={item.img} />
+                  <div>
+                    <h1 className='title'>{item.title}</h1>
+                    <p className='card-paragraph'>{item.details}</p>
+                  </div>
                 </div>
-                <div className="col details">
-                  <h2>{data.title}</h2>
-                  <p>{data.details}</p>
-                </div> */}
+              </div>
+            </>
+            ))
+          }
+
+          {/* <div className="card-wrapper">
+            <div className='card-container-box'>
+              <img className='card-icon-img' src={group1} />
+              <div>
+                <h1 className='title'>Main Examination</h1>
+                <p className='card-paragraph'>Geography is one of the most popular options. It owes this to its multidisciplinary characteristics.</p>
               </div>
             </div>
-          );
-        })}
+          </div>
+
+          <div className="card-wrapper">
+            <div className='card-container-box'>
+              <img className='card-icon-img' src={group1} />
+              <div>
+                <h1 className='title'>Main Examination</h1>
+                <p className='card-paragraph'>Geography is one of the most popular options. It owes this to its multidisciplinary characteristics.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="card-wrapper">
+            <div className='card-container-box'>
+              <img className='card-icon-img' src={group1} />
+              <div>
+                <h1 className='title'>Main Examination</h1>
+                <p className='card-paragraph'>Geography is one of the most popular options. It owes this to its multidisciplinary characteristics.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="card-wrapper">
+            <div className='card-container-box'>
+              <img className='card-icon-img' src={group1} />
+              <div>
+                <h1 className='title'>Main Examination</h1>
+                <p className='card-paragraph'>Geography is one of the most popular options. It owes this to its multidisciplinary characteristics.</p>
+              </div>
+            </div>
+          </div> */}
+
+        </div>
       </div>
-    </div>
+    </div >
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <div>
+    //   <h1 className="text-center fs-1 my-5">
+    //     Achieve Excellence with <br /> Our Programmes
+    //   </h1>
+
+
+    //   <div className="">
+    //     <>
+    //     {data.map((data, index) => {
+    //       return (
+    //         <div key={index} className="col-lg-6 col-md-6">
+    //           <div className="row">
+    //             <div className="left card-bg"></div>
+    //             <div className="right"></div>
+    //             {/* <div className="col card-img">
+    //               <img src={data.img} alt="img" />
+    //             </div>
+    //             <div className="col details">
+    //               <h2>{data.title}</h2>
+    //               <p>{data.details}</p>
+    //             </div> */}
+    //           </div>
+    //         </div>
+    //       );
+    //     })}
+    //     </>
+    //   </div>
+
+
+
+    // </div>
   );
 };
 
